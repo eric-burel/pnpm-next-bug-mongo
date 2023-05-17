231ba56acbf6858accc599916e22a0ee4c86e882
eric-burel <eb@lbke.fr>
2023-05-17 07:39:40 +0200

    add local package to ignored packages, no effect

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        // it has no effect on the mongodb warning
+        serverComponentsExternalPackages: ['db'],
+    },
+
+}
 
 module.exports = nextConfig
